2977de49dc9bad8b819d7f8404fe9d6eef54ed7a
Cojproct <cojproct@ucsc.edu>
2023-11-02 19:23:00 -0700

    lab 5

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - to create a program of pseudocode for rock,paper, scissors
+// Author: Cody Proctor
+// Date: 10/23/23
 
 // Constants
 
@@ -8,8 +8,35 @@
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
-  // some code here
-  // return results;
+ // Making macaroni & cheese roux style:
+//Mise en place (everything in its place)
+//Make sure you have all the ingredients ready (milk, butter, macaroni boxes, salt, and cheese powder)
+//All cookware is clean and ready for use, and easily accessible
+//Boil water
+//Add salt
+//Add the macaroni
+//Keep the water at a boil until the macaroni is ready
+//Turn the sink water on to cold and let it run down the drain during this next step
+//Pour the macaroni into a strainer to remove the water
+//Put butter in the pot and let it melt
+//Add cheese powder (remember to fold the packets up over the pot after it is emptied to create less mess)
+//Carefully pour milk into the pot, little bits at a time
+//Mix the butter, milk, and cheese powder until it reaches desired consistency. Repeat step 10 as necessary to reach this consistency
+//That creates the roux. Add the macaroni to the roux and stir from the bottom to coat the macaroni in the cheese roux
+//Serve and enjoy
+
+  // Pseudocode for Rock, Paper, Scissors:
+//Get all the variables: Computer choice, Player choice, and result
+//Assign each choice a number 1= rock, 2= paper, 3=scissors 
+//Have player choose 1, 2, or 3
+//Have computer generate random number 1-3
+//To determine the winner:
+	//1. If the player’s choice is the same as the computers choice then the result is a tie. 
+	//2. If the player’s choice is 3 (scissors) and the computers choice is 2 (paper) the result is player wins.
+	//3. If player choice is 2 (paper) and the computers choice is 1 (rock) then the result is player wins 
+	//4. If the player choice is 1 (rock) and the computer choice is 3 (scissors)the result is player wins 
+	//5. If any other combination occurs, the result is that the computer wins
+//print results
 }
 
 function main() {
--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,12 +1,29 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Data Types and Variables
+
+// Author: Cody Proctor
+// Date: oct 27, 2023
 
 // Constants
 
-// Functions
+// difine variables
+var make = "Ford";
+var model = "Mustang";
+var color = "red";
+var year = 1969;
+var ownIt = true;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
+
+// calculate
+age = 2023 - year
+
+
+//output
+document.writeln("Make: " + make + "<br>")
+document.writeln("model: " + model + "<br>")
+document.writeln("color: " + color + "<br>")
+document.writeln("year: " + year + "<br>")
+document.writeln("age: " + age + "<br>")
+
 function myFunction(param1, param2) {
   // some code here
   // return results;
@@ -14,7 +31,7 @@ function myFunction(param1, param2) {
 
 function main() {
   console.log("Main function started.");
-  // the code that makes everything happen
+ 
 }
 
 // let's get this party started
